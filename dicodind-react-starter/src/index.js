75e96e27da374568559f4e955d635c200821bff9
Fahmi Muhazir <fahmimuhazir12@gmail.com>
2024-10-09 00:06:51 +0700

    feat: react starter

--- dicodind-react-starter/src/index.js
+++ dicodind-react-starter/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
+import DicodingLogo from './dicoding_logo.png';
 const element = (
   <div>
     <h1>Biodata Perusahaan</h1>
@@ -8,6 +8,7 @@ const element = (
       <li>Nama: Dicoding Indonesia</li>
       <li>Bidang: Education</li>
       <li>Tagline: Decode Ideas, Discover Potential</li>
+      <img src={DicodingLogo} alt="Dicoding Logo" />
     </ul>
   </div>
 );
